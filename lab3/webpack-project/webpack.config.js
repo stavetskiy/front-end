4c200e00be0a6f272a69d1713435fc52363d6755
stavetskiy <137418083+stavetskiy@users.noreply.github.com>
2024-04-16 12:03:25 +0300

    Update webpack.config.js

--- lab3/webpack-project/webpack.config.js
+++ lab3/webpack-project/webpack.config.js
@@ -1,6 +1,15 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+function getPages() {
+  const pagesPath = path.resolve(__dirname, 'src', 'pages');
+  return fs.readdirSync(pagesPath).filter(file => file.endsWith('.html'));
+}
 
 module.exports = {
   entry: './src/index.js',
@@ -9,30 +18,26 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/pages/index.html',
-      filename: 'index.html',
-      chunks: ['main'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/pages/about.html',
-      filename: 'about.html',
-      chunks: ['about'],
-    }),
     new CopyWebpackPlugin({
       patterns: [
           {
-              from: path.resolve(__dirname, 'src/assets/images'),
-              to: path.resolve(__dirname, 'dist/assets/images')
-          }
-      ]
-     }),
+              from: path.resolve(__dirname, 'src', 'assets', 'images'),
+              to: path.resolve(__dirname, 'dist', 'assets', 'images'),
+          },
+      ],
+  }),
+    ...getPages().map(page => {
+        return new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'pages', page),
+            filename: page, 
+        });
+    }),
+    new MiniCssExtractPlugin(), 
   ],
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist'),
     },   
-     port: 8080,
+    port: 8080,
   },
 };
